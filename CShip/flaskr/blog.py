2dd836e0450a6bbf6ea6cf916bf3547562ad8472
happybear1234 <18225056301@163.com>
2020-11-29 22:47:08 +0800

    导航栏重新设置

--- CShip/flaskr/blog.py
+++ CShip/flaskr/blog.py
@@ -1,26 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template, request
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def base():
-    products = ["iphoneX", "MacBook Pro", "Huawei"]
-    kwargs = {
-        "products": products
-    }
-    return render_template('base.html', **kwargs)
+def basic():
+    return render_template('basic.html')
 
 
 @app.route('/calculate')
 def calculate():
-    return render_template('calculate.html')
+    if request.method == 'GET':
+        return render_template('calculate.html')
 
+@app.route('/brigade')
+def brigade():
+    if request.method == 'GET':
+        return render_template('brigade.html')
 
-@app.route('/ex')
-def my_test():
-    return render_template('test.html')
+@app.route('/experiment')
+def experiment():
+    if request.method == 'GET':
+        return render_template('experiment.html')
 
+@app.route('/mars')
+def mars():
+    if request.method == 'GET':
+        return render_template('mars.html')
+
+@app.route('/MDT')
+def MDT():
+    if request.method == 'GET':
+        return render_template('MDT.html')
+
+@app.route('/MTBF')
+def MTBF():
+    if request.method == 'GET':
+        return render_template('MTBF.html')
+
+@app.route('/MTTR')
+def MTTR():
+    if request.method == 'GET':
+        return render_template('MTTR.html')
+
+# @app.route('/')
+# def base():
+#     return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
